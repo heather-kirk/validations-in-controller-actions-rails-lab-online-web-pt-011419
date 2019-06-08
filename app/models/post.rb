20a9a9893b8daf63a5843e2d33d5637ca7ec1353
Heather Kirk <hskirk@gmail.com>
2019-06-08 02:47:13 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validate :title, presence: true
-  validate :category, inclusion:{in:%w(Fiction Non-Fiction)}
-  validate :content, length:{minimum:100}
+  validates :title, presence: true
+  validates :category, inclusion:{in:%w(Fiction Non-Fiction)}
+  validates :content, length:{minimum:100}
 end
